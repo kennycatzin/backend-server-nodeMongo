eae760aba73cba454327d36b542abcd68ced5462
kenn2506@gmail.com <kenn2506@gmail.com>
2019-07-17 14:26:17 -0500

    usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // Require
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
+
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexxion a la BD
 mongoose.connect('mongodb://localhost:27017/erp', (err, res) => {
@@ -11,14 +22,10 @@ mongoose.connect('mongodb://localhost:27017/erp', (err, res) => {
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 
 });
-
 //Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: ' Peticion realizada correctamente'
-    })
-});
+app.use('/', appRoutes);
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
 
 app.listen(3000, () => {
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
---
+++ middlewares/autenticacion.js
@@ -0,0 +1,21 @@
+var express = require('express');
+//bcryot repositorio github
+var router = express.Router();
+var jwt = require('jsonwebtoken');
+var SEED = require('./../config/config').SEED;
+//verificar token
+exports.verificaToken = function(req, res, next) {
+    var token = req.query.token;
+    jwt.verify(token, SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                mensaje: 'token incorrecto',
+                errors: err
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+
+    })
+}
